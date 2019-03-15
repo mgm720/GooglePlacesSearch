162f920a232b7c5857b394e5844b18b7a615f18d
Michael Miles <michael.gage.miles@gmail.com>
2019-03-14 21:10:01 -0700

    Added DataModel for the Place that will be returned.

---
+++ GooglePlacesSearch/Model/PlaceDataModel.swift
@@ -0,0 +1,11 @@
+//
+//  PlaceDataModel.swift
+//  GooglePlacesSearch
+//
+//  Created by Michael Miles on 3/14/19.
+//  Copyright © 2019 Michael Miles. All rights reserved.
+//
+
+import Foundation
+
+
