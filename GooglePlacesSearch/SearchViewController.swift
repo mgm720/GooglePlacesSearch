e76b46a80f6504b9d0fbafe944ef785d4e4a89ab
Michael Miles <michael.gage.miles@gmail.com>
2019-03-14 20:18:46 -0700

    Added TableView, created animation to move searchbar, and added UI label for user instructions.

--- GooglePlacesSearch/SearchViewController.swift
+++ GooglePlacesSearch/SearchViewController.swift
@@ -8,21 +8,53 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource {
+    
+    
 
     @IBOutlet weak var searchTextField: UITextField!
     @IBOutlet weak var searchInsetView: UIView!
+    @IBOutlet weak var searchLabel: UILabel!
+    @IBOutlet weak var resultsTableView: UITableView!
     
+    @IBOutlet weak var searchTextFieldTopConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupUI()
+        
+        resultsTableView.delegate = self
+        resultsTableView.dataSource = self
+        searchTextField.delegate = self
+        
+        resultsTableView.isHidden = true
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        
+        print("Began editing")
+        searchTextFieldTopConstraint.constant = 10
+        UIView.animate(withDuration: 0.5) {
+            self.view.layoutIfNeeded()
+            self.searchLabel.isHidden = true
+            self.resultsTableView.isHidden = false
+        }
     }
     
     func setupUI() {
         searchTextField.leftView = searchInsetView
         searchTextField.leftViewMode = .always
+    }
+    
+    //MARK: TableView methods
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ResultCell", for: indexPath)
+        return cell
     }
 
 
