d3ac41ba861dbc3ed22a0610bcd928ebd0fc6086
Michael Miles <michael.gage.miles@gmail.com>
2019-03-14 19:18:40 -0700

    Renamed VC to SearchVC

--- GooglePlacesSearch/SearchViewController.swift
+++ GooglePlacesSearch/SearchViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SearchViewController: UIViewController {
 
     @IBOutlet weak var searchTextField: UITextField!
     @IBOutlet weak var searchInsetView: UIView!
